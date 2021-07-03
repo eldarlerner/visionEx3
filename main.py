e67d1b677dff37fe834309f25558f3f020d1e19d
Your Name <you@example.com>
2021-07-03 21:15:49 +0300

    finish ophir please submit

--- main.py
+++ main.py
@@ -25,11 +25,6 @@ def DLT(worldPoints, planePoints):
     M = build_M(worldPoints, planePoints)
     u,sig,VT = np.linalg.svd(M)
     p = VT[-1].reshape(3,4)
-    hom = findHomography(worldPoints[:,:3],planePoints)[0]
-    print(hom @ worldPoints[0][:3])
-    print()
-    print(p @ worldPoints[0])
-    # x = np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]])
     H = p[:3, :3]
     h = p[:,3:]
     H_inv = np.linalg.inv(H)
@@ -49,14 +44,7 @@ def DLT(worldPoints, planePoints):
 if __name__ == '__main__':
     im1 = np.array(
         [[430,521,1], [763,708, 1], [664,408, 1], [967,551, 1],[367,86, 1], [758,184, 1], [627,34, 1], [995,98, 1]])
-    # im2 = np.array(
-    #     [[1193, 878, 1], [890, 2136, 1], [2569, 915, 1], [2809, 2161, 1]])
-    # im3 = np.array(
-    #     [[1861, 785, 1], [619, 1398, 1], [3301, 1206, 1], [2451, 2367, 1]])
     real = np.array([[0,0,0,1],[1,0,0,1],[0,0.7,0,1],[1,0.7,0,1],[0,0,0,1],[1,0,1,1],[0,0.7,1,1],[1,0.7,1,1]])
     translation,rotation = DLT(worldPoints=real,planePoints=im1)
-    # print(rotation)
-    # print()
-    # print(translation)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
